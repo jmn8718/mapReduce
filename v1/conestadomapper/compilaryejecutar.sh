1d63dbe6e477441ffa46ca28d723f8619d63d95e
Jose Miguel Navarro <jmn8718@gmail.com>
2015-01-24 01:28:05 +0100

    deleted useless files

--- v1/conestadomapper/compilaryejecutar.sh
+++
@@ -1,4 +0,0 @@
-rm -r output*
-javac  -cp `hadoop classpath` *.java  # compila
-jar cvf LogCounter.jar *.class # crea el JAR
-hadoop jar LogCounter.jar LogCounter -D excluido=127.0.0.2 input* output
